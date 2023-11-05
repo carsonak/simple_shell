0a8525deaff7c10dc6518e37f0c9254b3812c373
Andrew <49104171+carsonak@users.noreply.github.com>
2023-11-05 14:48:45 +0300

    source code and binary file

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-simple_shell
+#### SIMPLE_SHELL
+
+Compile command: gcc -Wall -Werror -Wextra -pedantic -std=gnu89 -g *.c -o simple_shell
--- scrap/prototype_shell.c
+++ scrap/prototype_shell.c
@@ -10,11 +10,17 @@
 int main(int argc, char *argv[])
 {
 	char *error_msg = NULL;
+	char **cmds;
 
 	if (argc == 1)
 	{
-		/*This if block should be replaced with a prompting loop*/
-		error_msg = "Error: Wrong usage.\nUse: ./p_sh [command] [options]\n";
+		/**
+		 * This if block should be replaced with a prompting loop that parses
+		 * input strings and records the string pointers in an array (cmds).
+		 * It then calls (executor) if the command exists.
+		 * Should also handle ^C [EOT] and ^D [EOF]
+		 */
+		error_msg = "Error: Wrong usage.\n./p_sh [path/to/binary/file] [options_to_executable]\n";
 		write(STDERR_FILENO, error_msg, (sizeof(*error_msg) * strlen(error_msg)));
 	}
 	else
@@ -22,15 +28,16 @@ int main(int argc, char *argv[])
 		/**
 		 * Should be able to detrmine if command exist via searching
 		 * the PATH environment variable before calling the executor.
-		 * */
-		executor(argv);
+		 */
+		cmds = &argv[1];
+		executor(cmds);
 	}
 
 	return (0);
 }
 
 /**
- * executor - forks the process and executes given command
+ * executor - forks the process and executes given command in child process
  * @cmds: an array of pointers to the command and its options
  *
  * Return: standard success int or standard failure int
@@ -39,7 +46,7 @@ int executor(char **cmds)
 {
 	pid_t frk1 = 0, frkerr = 0;
 	int err = 0, status;
-	char **nwenviron = __environ, *error_msg = NULL;
+	char **nwenviron = __environ;
 
 	/*Forking the processes and recording the PIDs*/
 	frk1 = fork();
@@ -51,32 +58,27 @@ int executor(char **cmds)
 		/**
 		 * Replacing the current program with the program or
 		 * script specified in cmds[0].
-		 * */
+		 */
 		err = execve(cmds[0], cmds, nwenviron);
 		if (err == -1)
 		{
-			perror("\nCouldn't execute");
+			perror("Couldn't execute");
 			exit(EXIT_FAILURE);
 		}
 	}
 	else
 	{
 		/*Parent waits for child to terminate and handles any errors*/
-		frkerr = waitpid(frk1, &status, WNOHANG);
+		frkerr = waitpid(frk1, &status, 0);
 		if (frkerr == -1)
-			perror("\nWait error");
-		else if (frkerr > 0)
+			perror("Wait error");
+		else
 		{
 			/*These dprintf statements should be replace with allowed functions*/
 			if (WIFEXITED(status))
-				dprintf(STDERR_FILENO, "%d: Exited with %d status\n", frkerr, WEXITSTATUS(status));
+				dprintf(STDERR_FILENO, "%d: Exited with status %d\n", frkerr, WEXITSTATUS(status));
 			else
 				dprintf(STDERR_FILENO, "%d: Exited abnormally", frkerr);
-		}
-		else
-		{
-			error_msg = "No children\n";
-			write(STDERR_FILENO, error_msg, (sizeof(*error_msg) * strlen(error_msg)));
 		}
 	}
 
