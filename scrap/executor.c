bf7e026d25a1cae0735bd02b60ce38710b804421
Andrew <49104171+carsonak@users.noreply.github.com>
2023-11-08 16:49:51 +0300

    Rearranged the files

---
+++ scrap/executor.c
@@ -0,0 +1,50 @@
+#include "scrap.h"
+
+/**
+ * executor - forks the process and executes given command in child process
+ * @cmds: an array of pointers to the command and its options
+ *
+ * Return: standard success int or standard failure int
+ */
+int executor(char **cmds)
+{
+	pid_t frk1 = 0, frkerr = 0;
+	int err = 0, status;
+	char **nwenviron = __environ;
+
+	/*Forking the processes and recording the PIDs*/
+	frk1 = fork();
+
+	if (frk1 == -1)
+		perror("Couldn't fork");
+	else if (frk1 == 0)
+	{
+		/**
+		 * Replacing the current program with the program or
+		 * script specified in cmds[0].
+		 */
+		err = execve(cmds[0], cmds, nwenviron);
+		if (err == -1)
+		{
+			perror("Couldn't execute");
+			exit(EXIT_FAILURE);
+		}
+	}
+	else
+	{
+		/*Parent waits for child to terminate and handles any errors*/
+		frkerr = waitpid(frk1, &status, 0);
+		if (frkerr == -1)
+			perror("Wait error");
+		else
+		{
+			/*These dprintf statements should be replace with allowed functions*/
+			if (WIFEXITED(status))
+				dprintf(STDERR_FILENO, "%d: Exited with status %d\n", frkerr, WEXITSTATUS(status));
+			else
+				dprintf(STDERR_FILENO, "%d: Exited abnormally", frkerr);
+		}
+	}
+
+	return (EXIT_SUCCESS);
+}
--- scrap/prototype_shell.c
+++ scrap/prototype_shell.c
@@ -9,19 +9,36 @@
  */
 int main(int argc, char *argv[])
 {
-	char *error_msg = NULL;
 	char **cmds;
+	char buf[1024];
+	int err;
 
 	if (argc == 1)
 	{
-		/**
-		 * This if block should be replaced with a prompting loop that parses
-		 * input strings and records the string pointers in an array (cmds).
-		 * It then calls (executor) if the command exists.
-		 * Should also handle ^C [EOT] and ^D [EOF]
-		 */
-		error_msg = "Error: Wrong usage.\n./p_sh [/path/to/binary/file] [options_to_executable]\n";
-		write(STDERR_FILENO, error_msg, (sizeof(*error_msg) * strlen(error_msg)));
+		while (1)
+		{
+			err = write(1, "$! ", 2);
+			if (err == -1)
+			{
+				perror("Whoops");
+				return (EXIT_FAILURE);
+			}
+			err = read(0, buf, 1024);
+			if (err == -1)
+			{
+				perror("Failed to read");
+				return (EXIT_FAILURE);
+			}
+
+			err = write(1, buf, strlen(buf));
+			if (err == -1)
+			{
+				perror("Failed to write");
+				return (EXIT_FAILURE);
+			}
+
+			memset(buf, '\0', 1024);
+		}
 	}
 	else
 	{
@@ -34,53 +51,4 @@ int main(int argc, char *argv[])
 	}
 
 	return (0);
-}
-
-/**
- * executor - forks the process and executes given command in child process
- * @cmds: an array of pointers to the command and its options
- *
- * Return: standard success int or standard failure int
- */
-int executor(char **cmds)
-{
-	pid_t frk1 = 0, frkerr = 0;
-	int err = 0, status;
-	char **nwenviron = __environ;
-
-	/*Forking the processes and recording the PIDs*/
-	frk1 = fork();
-
-	if (frk1 == -1)
-		perror("Couldn't fork");
-	else if (frk1 == 0)
-	{
-		/**
-		 * Replacing the current program with the program or
-		 * script specified in cmds[0].
-		 */
-		err = execve(cmds[0], cmds, nwenviron);
-		if (err == -1)
-		{
-			perror("Couldn't execute");
-			exit(EXIT_FAILURE);
-		}
-	}
-	else
-	{
-		/*Parent waits for child to terminate and handles any errors*/
-		frkerr = waitpid(frk1, &status, 0);
-		if (frkerr == -1)
-			perror("Wait error");
-		else
-		{
-			/*These dprintf statements should be replace with allowed functions*/
-			if (WIFEXITED(status))
-				dprintf(STDERR_FILENO, "%d: Exited with status %d\n", frkerr, WEXITSTATUS(status));
-			else
-				dprintf(STDERR_FILENO, "%d: Exited abnormally", frkerr);
-		}
-	}
-
-	return (EXIT_SUCCESS);
 }
