8786348018c778b39da059d4ab3ffe90bae39913
mkaran12 <mosesjnr46466@gmail.com>
2023-11-15 00:14:07 -0800

    betty checker

--- betty_check.c
+++ betty_check.c
@@ -2,13 +2,11 @@
 /**
  * add -Adds two integers
  * @num1 : the first integer to be added
- * @num2 : the second ineger to be added
- * 
+ * @num2 : the second ineger to be added 
  * return: The result of the addition.
- * 
 */
-
 int  add(int num1, int num2)
 {
     return (num1 + num2);
 }
+
