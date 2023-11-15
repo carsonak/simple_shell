cacd4b12333c1fe3dbdbb33873daf6b034075646
Andrew <49104171+carsonak@users.noreply.github.com>
2023-11-15 18:10:35 +0300

    Better interactive mode

--- main.c
+++ main.c
@@ -14,34 +14,56 @@ int main(int argc, char *argv[])
 
 	if (argc == 1)
 	{
-		while (1)
+		errno = 0;
+		err = isatty(STDIN_FILENO);
+		if (err)
 		{
-			err = write(1, "$!", 2);
-			if (err == -1)
+			while (1)
 			{
-				perror("Whoops");
-				return (EXIT_FAILURE);
-			}
+				err = write(1, "$!", 2);
+				if (err == -1)
+				{
+					perror("Whoops");
+					return (EXIT_FAILURE);
+				}
 
-			cmds = parser(cmds);
-			if (cmds)
-			{
-				/*Search the PATH for directory with the file before executing*/
-				flush_io();
-				executor(cmds);
-				free_args(cmds);
+				cmds = parser(cmds);
+				if (cmds)
+				{
+					/*Search the PATH for directory with the file before executing*/
+					flush_io();
+					executor(cmds);
+					free_args(cmds);
+				}
+				else if (err == -1)
+					write(STDERR_FILENO, "Could not parse command\n", 25);
 			}
-			else if (err == -1)
-				write(STDERR_FILENO, "Could not parse command\n", 25);
 		}
-	}
+		else
+		{
+			if (errno == ENOTTY)
+			{
+				cmds = parser(cmds);
+				if (cmds)
+				{
+					/*Search the PATH for directory with the file before executing*/
+					flush_io();
+					executor(cmds);
+					free_args(cmds);
+				}
+				else if (err == -1)
+					write(STDERR_FILENO, "Could not parse command\n", 25);
+			}
+			else
+				perror("Corrupted input");
+		}
+		}
 	else
 	{
 		/**
 		 * Should be able to detrmine if command exist via searching
 		 * the PATH environment variable before calling the executor.
 		 */
-
 		cmds = &argv[1];
 		executor(cmds);
 	}
