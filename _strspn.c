726d57ba3f95806bf0f0fec9b77aed89a18f8ed2
Andrew <49104171+carsonak@users.noreply.github.com>
2023-12-17 01:16:31 +0300

    typo

--- _strspn.c
+++ _strspn.c
@@ -2,7 +2,7 @@
 
 /**
  * _strspn - checks the first occurence of a sub-string in a string
- * @s: String to be checked
+ * @s: string to be checked
  * @accept: sub-string to check
  *
  * Return: the length of the first occurences
